2415a550dc53a268c8c7a9b6763be2ac3976f164
박정웅 <lgchu10@naver.com>
2025-04-13 01:59:29 +0900

    2025-04-13 01:59

--- C++Tutorial/counters.cpp
+++ C++Tutorial/counters.cpp
@@ -6,16 +6,33 @@ using namespace std;
 
 void worker();
 
-void worker() {
-	for (int i = 0; i < 10; ++i) {
-		Sleep(1000);
-		cout << i << endl;
-	}
-}
+///예제1: join()
+//void worker() {
+//	for (int i = 0; i < 10; ++i) {
+//		Sleep(1000);
+//		cout << i << endl;
+//	}
+//}
+//
+//int main() {
+//	thread t1(worker); //새로운 스레드 t1을 만들어서 worker 함수를 실행하라는 뜻.
+//	t1.join(); //t1 스레드가 끝날 때까지 메인 프로그램을 기다리게 함.
+//	printf("===end of main()===\n");
+//	return 0;
+//}
 
-int main() {
-	thread t1(worker);
-	t1.join();
-	printf("===end of main()===\n");
-	return 0;
+
+///예제2: detach()
+void worker() { 
+	for (int i = 0; i < 10; ++i) { 
+		Sleep(1000); 
+		cout << i << endl; 
+	} 
+}
+int main() { 
+	thread t1(worker); 
+	t1.detach(); //main함수와 연결되지 않음. main이 끝나더라도, 분리된 스레드는 시스템이 관리하며 계속 실행될 수 있음. (단, 프로그램이 완전히 종료되면 스레드도 종료됨).
+	printf("===end of main()===\n"); 
+	Sleep(5000); 
+	return 0; 
 }
